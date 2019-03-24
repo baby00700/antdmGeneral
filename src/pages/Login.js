e9a0a2a4c1c40f34a6facb70675b55603c692668
zhaih <1014504021@qq.com>
2019-03-24 13:31:02 +0800

    add login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,82 @@
+import React from 'react'
+import styles from './Login.less';
+import logo from '../assets/logo.png'
 
-import styles from './Login.css';
+class Login extends React.Component{
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page Login</h1>
-    </div>
-  );
+  state = {
+    teacher: false
+  }
+
+  stuLoginDoc = () => {
+    return (
+      <div className={styles.normal}>
+        <div className={styles.logo}>
+          <img src={logo} alt="" style={{width:'70%'}}/>
+        </div>
+        <div className={styles.loginForm}>
+          <div className={styles.userName}>
+            <div className={styles.inputs}>
+              <input type="text" placeholder='请输入学号'/>
+            </div>
+          </div>
+          <div className={styles.password}>
+            <div className={styles.inputs}>
+              <input type="password" placeholder='请输入密码'/>
+            </div>
+          </div>
+          <div className={styles.buttons}>登录</div>
+        </div>
+        <div className={styles.teacherLogin}>
+          <span onClick={() => {
+            this.setState({
+              teacher: true
+            })
+          }}>教职工登录</span> | <span>忘记密码</span>
+        </div>
+      </div>
+    )
+  }
+
+  teaLoginDoc = () => {
+    return (
+      <div className={styles.normalt}>
+        <div className={styles.logo}>
+          <img src={logo} alt="" style={{width:'70%'}}/>
+        </div>
+        <div className={styles.loginForm}>
+          <div className={styles.userName}>
+            <div className={styles.inputs}>
+              <input type="text" placeholder='请输入工号'/>
+            </div>
+          </div>
+          <div className={styles.password}>
+            <div className={styles.inputs}>
+              <input type="password" placeholder='请输入密码'/>
+            </div>
+          </div>
+          <div className={styles.buttons}>登录</div>
+        </div>
+        <div className={styles.teacherLogin}>
+          <span onClick={() => {
+            this.setState({
+              teacher:false
+            })
+          }}>学生登录</span> | <span>忘记密码</span>
+        </div>
+      </div>
+    )
+  }
+
+  docs = () => {
+    const {teacher} = this.state
+    return !teacher ? this.stuLoginDoc() : this.teaLoginDoc()
+  }
+
+  render () {
+    return this.docs()
+  }
 }
+
+
+export default Login
